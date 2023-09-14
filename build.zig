7822890899976506e04c4646d51412133101415d
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-09-14 12:16:13 -0400

    try using addInstallHeaderFile instead of installDirectory

--- build.zig
+++ build.zig
@@ -1,9 +1,28 @@
 const std = @import("std");
 
+const headers = &[_][]const u8{
+    "args.h",
+    "chrono.h",
+    "color.h",
+    "compile.h",
+    "core.h",
+    "format-inl.h",
+    "format.h",
+    "os.h",
+    "ostream.h",
+    "printf.h",
+    "ranges.h",
+    "std.h",
+    "xchar.h",
+};
+
 pub fn build(b: *std.Build) !void {
-    b.installDirectory(.{
-        .source_dir = .{ .path = "include" },
-        .install_dir = .header,
-        .install_subdir = "",
-    });
+    var inst_step = b.getInstallStep();
+    for (headers) |header| {
+        const h_step = b.addInstallHeaderFile(
+            std.fs.path.join(b.allocator, &.{ "include", "fmt", header }) catch @panic("OOM"),
+            std.fs.path.join(b.allocator, &.{ "fmt", header }) catch @panic("OOM"),
+        );
+        inst_step.dependOn(&h_step.step);
+    }
 }
